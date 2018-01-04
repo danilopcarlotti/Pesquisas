fe650191516f305e9035b170e721872fabfe3684
danilo <danilopcarlotti@gmail.com>
2018-01-04 12:48:03 -0200

    atualização

--- conexao_local.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-
-def cursorConexao():
-	connection = pymysql.connect(host='localhost',user='root',password='danilo11',db='contratos_governo_federal',charset='utf8', autocommit = True)
-	return connection.cursor()
-
-if __name__ == '__main__':
-	pass
