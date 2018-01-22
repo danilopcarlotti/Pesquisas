3cf9aa9ba4fdec0a92eb1305430428a53d4f292d
digesto20 <danilopcarlotti@gmail.com>
2018-01-22 17:20:03 -0200

    atualização

--- crawlers/crawler_processos_stf.py
+++ crawlers/crawler_processos_stf.py
@@ -11,34 +11,13 @@ class processosSTF(crawlerJus):
 	def __init__(self):
 		super().__init__()
 		ssl._create_default_https_context = ssl._create_unverified_context 
+		self.link_base = 'http://www.stf.jus.br/portal/processo/verProcessoAndamento.asp?incidente='
+		self.numero_final = 6000000 #Este é o último número estimado de um processo válido no STF em 11/11/2017. Número deve ser atualizado
+		self.numero_inicial = 4806730#1169086 até 3000000
 
 		
 	def baixarDadosProcesso(self):
 		pass
-
-	def baixarVotos(self):
-		pass
-
-	def baixarDocumentos(self):
-		pass
-		
-
-if __name__ == '__main__':
-	pass
-
-import threading,re
-from crawlerJus import crawlerJus
-from multiprocessing import Queue
-from bs4 import BeautifulSoup
-from conexao_local import cursorConexao
-
-class crawlerSTF(crawlerJus):
-	"""Crawler para download de metadados"""
-	def __init__(self):
-		crawlerJus.__init__(self)
-		self.link_base = 'http://www.stf.jus.br/portal/processo/verProcessoAndamento.asp?incidente='
-		self.numero_final = 6000000 #Este é o último número estimado de um processo válido no STF em 11/11/2017. Número deve ser atualizado
-		self.numero_inicial = 4806730#1169086 até 3000000
 
 	def baixarVotos(self,link):
 		pagina = self.baixa_pag(link)
@@ -63,14 +42,32 @@ class crawlerSTF(crawlerJus):
 								if texto_final:
 									cursor = cursorConexao()
 									cursor.execute('INSERT INTO processos_stf.texto_decisoes (link_pagina, texto_decisao) values("%s","%s");' % (link,texto_final))
-	
+
 	def baixa_decisoes_proc(self):
 		contador = 0
 		for i in range(self.numero_inicial,self.numero_final):
 			self.baixarVotos(self.link_base+str(i))
 			if i % 1000 == 0:
 				print(i)
-			
-if __name__ == '__main__':
-	c = crawlerSTF()
-	c.baixa_decisoes_proc()
+
+	def baixarDocumentos(self):
+		pass
+		
+
+# if __name__ == '__main__':
+# 	pass
+
+cursor = cursorConexao()
+cursor.execute('SELECT id, link_dados from stf.dados_processo limit 1000000;')
+links_pai = cursor.fetchall()
+links_pai_d = {}
+for id_p,link in links_pai:
+	links_pai_d[int(link.split('incidente=')[1])] = int(id_p)
+
+cursor.execute('SELECT id, link_pagina from stf.texto_decisoes limit 1000000;')
+links = cursor.fetchall()
+for id_l,link_pag in links:
+	try:
+		cursor.execute('UPDATE stf.texto_decisoes set id_processo = ("%s") where id = ("%s")' % (links_pai_d[int(link_pag.split('incidente=')[1])],id_l))
+	except:
+		pass
