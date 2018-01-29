41d2ff178772c2661d021a592bc5d800fa52304b
digesto20 <danilopcarlotti@gmail.com>
2018-01-29 17:24:48 -0200

    atualização

--- crawlers/crawler_jurisprudencia_tj.py
+++ crawlers/crawler_jurisprudencia_tj.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from common.conexao_local import cursorConexao
 from datetime import date
-import time, os, common.download_path, re
+import time, os, common.download_path, re, subprocess
 
 class crawler_jurisprudencia_tj():
 	"""Generic class with methods for crawler_jurisprudencia_tj's"""
@@ -14,9 +14,11 @@ class crawler_jurisprudencia_tj():
 		self.lista_meses = ['0'+str(i) for i in range(1,10)]
 		self.lista_meses += ['10','11','12']
 
-	def captcha(self):
+	def captcha(self, path=None):
 		from common.transcrever_audio import transcrever_audio
 		t = transcrever_audio()
+		if path:
+			return t.transcrever(path)
 		for file in os.listdir(common.download_path.path+'/'):
 			if re.search(r'wav',file):
 				return t.transcrever(audio=common.download_path.path+'/'+file)
@@ -25,6 +27,35 @@ class crawler_jurisprudencia_tj():
 		for file in os.listdir(common.download_path.path+'/'):
 			if re.search(r'wav',file):
 				os.remove(common.download_path.path+'/'+file)
+
+	def download_pdf_acordao(self,link,input_captcha_xpath,ouvir_captch_xpath,id_acordao):
+		driver = webdriver.Chrome(self.chromedriver)
+		driver.get_link(link)
+		try:
+			driver.find_element_by_xpath(input_captcha_xpath).click() #VERIFICAR SE ESTE É UM TESTE POSSÍVEL E/OU BOM
+			self.delete_audios()
+			# alterar o identificador do monitor de stereo
+			# https://www.funwithelectronics.com/?id=95
+			command = 'pacat --record -d alsa_output.pci-0000XXX.monitor | sox -t raw -r 44100 -s -L -b 16 -c 2 - "audio.wav" trim 0 5'
+			proc = subprocess.Popen(command, shell=True)
+			driver.find_element_by_xpath(ouvir_captch_xpath).click()
+			try:
+				outs, errs = proc.communicate(timeout=6)
+			except TimeoutExpired:
+				proc.kill()
+				outs, errs = proc.communicate()
+			driver.find_element_by_xpath(input_captcha_xpath).send_keys(self.captcha())
+			self.delete_audios()
+		except:
+			pass
+		pyautogui.hotkey('ctrl','s')
+		time.sleep(1)
+		pyautogui.typewrite('Acordao_'+id_acordao)
+		time.sleep(1)
+		pyautogui.press('enter')
+		time.sleep(1)
+		driver.close()
+
 
 	def download_jurisprudencia(self,driver,pesquisa_livre,data_julg_iniXP,data_julg_ini,data_julg_fimXP,data_julg_fim,botao_pesquisar,termo):
 		driver.find_element_by_xpath(pesquisa_livre).send_keys(termo)
