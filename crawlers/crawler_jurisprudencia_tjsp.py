0e406cd41beb4aaa49c8044af5cd947e75fb46b5
danilo <danilopcarlotti@gmail.com>
2018-02-07 10:27:32 -0200

    atualização

--- crawlers/crawler_jurisprudencia_tjsp.py
+++ crawlers/crawler_jurisprudencia_tjsp.py
@@ -1,11 +1,10 @@
 import sys, re, os
-from bs4 import BeautifulSoup
 from crawler_jurisprudencia_tj import crawler_jurisprudencia_tj
-from common.conexao_local import cursorConexao
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class crawler_jurisprudencia_tjsp(crawler_jurisprudencia_tj):
+class crawler_jurisprudencia_tjsp():
 	"""Crawler especializado em retornar textos da jurisprudência de segunda instância de São Paulo"""
 	def __init__(self):
 		crawler_jurisprudencia_tj.__init__(self)
@@ -19,27 +18,19 @@ class crawler_jurisprudencia_tjsp(crawler_jurisprudencia_tj):
 		self.tabela_colunas = 'justica_estadual.jurisprudencia_sp (ementas)'
 		self.link_esaj = 'https://esaj.tjsp.jus.br/cjsg/getArquivo.do?cdAcordao=%s&cdForo=%s'
 
-	def download_acordao_sp(self,id_acordao,link):
-		crawler_jurisprudencia_tj.download_pdf_acordao(self,link,'//*[@id="valorCaptcha"]','//*[@id="captchaInfo"]/ul/li[1]/a','//*[@id="pbEnviar"]','sp_2_inst_' + id_acordao)
-
-def main():
-	c = crawler_jurisprudencia_tjsp()
-	cursor = cursorConexao()
-	cursor.execute('SELECT id,ementas from justica_estadual.jurisprudencia_sp limit 10000000;')
-	lista_links = cursor.fetchall()
-	for i,l in lista_links:
-		c.download_acordao_sp(i,l)
-	# print('comecei ',c.__class__.__name__)
-	# try:
-	# 	for l in range(len(c.lista_anos)):
-	# 		print(c.lista_anos[l],'\n')
-	# 		for m in range(len(c.lista_meses)):
-	# 			try:
-	# 				crawler_jurisprudencia_tj.download_tj_ESAJ(c,crawler_jurisprudencia_tj,'01'+c.lista_meses[m]+c.lista_anos[l],'28'+c.lista_meses[m]+c.lista_anos[l],termo='a')
-	# 			except Exception as e:
-	# 				print(e)
-	# except Exception as e:
-	# 	print('finalizei o ano com erro ',e)
+	def download_acordao_sp(link,id_acordao):
+		self.download_pdf_acordao(link,'//*[@id="valorCaptcha"]','//*[@id="captchaInfo"]/ul/li[1]/a',id_acordao)
 
 if __name__ == '__main__':
-	main()
+	c = crawler_jurisprudencia_tjsp()
+	print('comecei ',c.__class__.__name__)
+	try:
+		for l in range(len(c.lista_anos)):
+			print(c.lista_anos[l],'\n')
+			for m in range(len(c.lista_meses)):
+				try:
+					crawler_jurisprudencia_tj.download_tj_ESAJ(c,crawler_jurisprudencia_tj,'01'+c.lista_meses[m]+c.lista_anos[l],'28'+c.lista_meses[m]+c.lista_anos[l],termo='a')
+				except Exception as e:
+					print(e)
+	except Exception as e:
+		print('finalizei o ano com erro ',e)
