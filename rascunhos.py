bbcfa9351c7948fa0b433c88b16e7343ef085a9a
danilo <danilopcarlotti@gmail.com>
2018-05-29 10:03:02 -0300

    atualização

--- rascunhos.py
+++ rascunhos.py
@@ -14,5 +14,5 @@ mNB = mNB_classification_text(dados)
 cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 5,1;')
 examples = cursor.fetchall()
 for e, class_e in examples:
-	print(e,mNB.test_mNB([e.encode('utf-8')]),(mNB.test_mNB([e.encode('utf-8')]) == class_e))
+	print(e.encode('utf-8'),mNB.test_mNB([e.encode('utf-8')]),(mNB.test_mNB([e.encode('utf-8')]) == class_e))
 # print(np.mean(acuracia))
