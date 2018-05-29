ebc4a45b9953ff9d91a7284bd579ad02cf818e2a
danilo <danilopcarlotti@gmail.com>
2018-05-29 09:23:11 -0300

    atualização

--- rascunhos.py
+++ rascunhos.py
@@ -1,11 +1,20 @@
-from common_nlp.pdf_to_text import pdf_to_text
-# from diarios_base import *
-import os
+from crawlers.common.conexao_local import cursorConexao
+from common_nlp.mNB_classification_text import mNB_classification_text
+import numpy as np
 
-contador = 0
-arqs_i = os.listdir('/home/danilo/Documentos/Diários_trt_2017')
-for arq in arqs_i:
-	arq_trt = open("/home/danilo/Documentos/Diários_trt_2017_txt/trt{}.txt".format(str(contador)),'a',encoding="utf-8")
-	arq_trt.write(pdf_2_txt.convert_pdfminer(i))
-	contador += 1
-	arq_trt.close()
+
+cursor = cursorConexao()
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 5;')
+dados = cursor.fetchall()
+acuracia = np.array([])
+for i in range(1,len(dados)-1):
+	dados_treino = dados[:i]+dados[i+1:]
+	dados_teste = dados[i]
+	mNB = mNB_classification_text(dados_treino)
+	for texto, class_t in dados_teste:
+		if (mNB.test_mNB([texto]) == class_t):
+			acuracia = 1
+		else:
+			acuracia = 0
+		acuracia.insert(int(classificacao))
+print(np.mean(acuracia))
