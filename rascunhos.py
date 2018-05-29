ba46ad7440bb18f0003de4b95cf0d9c04dc94c2f
danilo <danilopcarlotti@gmail.com>
2018-05-29 10:07:09 -0300

    atualização

--- rascunhos.py
+++ rascunhos.py
@@ -9,11 +9,14 @@ cursor = cursorConexao()
 cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 50;')
 dados = cursor.fetchall()
 print(len(dados))
-# acuracia = np.array([])
+acuracia = np.array([])
 mNB = mNB_classification_text(dados)
 
 cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 51,1;')
 examples = cursor.fetchall()
 for e, class_e in examples:
-	print(e.encode('utf-8'),mNB.test_mNB([e.encode('utf-8')]),(mNB.test_mNB([e.encode('utf-8')]) == class_e))
-# print(np.mean(acuracia))
+	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+		acuracia.insert(1)
+	else:
+		acuracia.insert(0)
+print(np.mean(acuracia))
