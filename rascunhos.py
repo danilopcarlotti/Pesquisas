90404f5681747b0334901f6ba08a64f3e46396f1
danilo <danilopcarlotti@gmail.com>
2018-05-29 10:18:50 -0300

    atualização

--- rascunhos.py
+++ rascunhos.py
@@ -6,17 +6,52 @@ import numpy as np
 
 
 cursor = cursorConexao()
-cursor.execute('SELECT count(*) from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null')
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 700;')
 dados = cursor.fetchall()
-print(dados)
-# acuracia = []
-# mNB = mNB_classification_text(dados)
+acuracia = []
+mNB = mNB_classification_text(dados)
 
-# cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 50,20;')
-# examples = cursor.fetchall()
-# for e, class_e in examples:
-# 	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
-# 		acuracia.append(1)
-# 	else:
-# 		acuracia.append(0)
-# print(np.mean(acuracia))
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 700,20;')
+examples = cursor.fetchall()
+for e, class_e in examples:
+	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+		acuracia.append(1)
+	else:
+		acuracia.append(0)
+print(np.mean(acuracia))
+
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 720,20;')
+examples = cursor.fetchall()
+for e, class_e in examples:
+	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+		acuracia.append(1)
+	else:
+		acuracia.append(0)
+print(np.mean(acuracia))
+
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 740,20;')
+examples = cursor.fetchall()
+for e, class_e in examples:
+	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+		acuracia.append(1)
+	else:
+		acuracia.append(0)
+print(np.mean(acuracia))
+
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 760,20;')
+examples = cursor.fetchall()
+for e, class_e in examples:
+	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+		acuracia.append(1)
+	else:
+		acuracia.append(0)
+print(np.mean(acuracia))
+
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 780,20;')
+examples = cursor.fetchall()
+for e, class_e in examples:
+	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+		acuracia.append(1)
+	else:
+		acuracia.append(0)
+print(np.mean(acuracia))
