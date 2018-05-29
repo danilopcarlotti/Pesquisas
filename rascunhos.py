3667cc3e6704051b61e8397564861cee9401de9a
danilo <danilopcarlotti@gmail.com>
2018-05-29 10:04:16 -0300

    atualização

--- rascunhos.py
+++ rascunhos.py
@@ -6,14 +6,14 @@ import numpy as np
 
 
 cursor = cursorConexao()
-cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null;')
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 50;')
 dados = cursor.fetchall()
 print(len(dados))
 # acuracia = np.array([])
-# mNB = mNB_classification_text(dados)
+mNB = mNB_classification_text(dados)
 
-# cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 5,1;')
-# examples = cursor.fetchall()
-# for e, class_e in examples:
-# 	print(e.encode('utf-8'),mNB.test_mNB([e.encode('utf-8')]),(mNB.test_mNB([e.encode('utf-8')]) == class_e))
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 51,1;')
+examples = cursor.fetchall()
+for e, class_e in examples:
+	print(e.encode('utf-8'),mNB.test_mNB([e.encode('utf-8')]),(mNB.test_mNB([e.encode('utf-8')]) == class_e))
 # print(np.mean(acuracia))
