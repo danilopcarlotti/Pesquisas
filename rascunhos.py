f03def0d3777e258062edcc10b14b04a6cf00032
danilo <danilopcarlotti@gmail.com>
2018-05-29 10:10:14 -0300

    atualização

--- rascunhos.py
+++ rascunhos.py
@@ -6,16 +6,17 @@ import numpy as np
 
 
 cursor = cursorConexao()
-cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 50;')
+cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null')
 dados = cursor.fetchall()
-acuracia = []
-mNB = mNB_classification_text(dados)
+print(dados)
+# acuracia = []
+# mNB = mNB_classification_text(dados)
 
-cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 50,20;')
-examples = cursor.fetchall()
-for e, class_e in examples:
-	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
-		acuracia.append(1)
-	else:
-		acuracia.append(0)
-print(np.mean(acuracia))
+# cursor.execute('SELECT texto_decisao, classificacao from jurisprudencia_2_inst.jurisprudencia_2_inst where classificacao is not null limit 50,20;')
+# examples = cursor.fetchall()
+# for e, class_e in examples:
+# 	if (mNB.test_mNB([e.encode('utf-8')]) == class_e):
+# 		acuracia.append(1)
+# 	else:
+# 		acuracia.append(0)
+# print(np.mean(acuracia))
