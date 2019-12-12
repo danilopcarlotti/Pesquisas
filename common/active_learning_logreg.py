78390977597298595b79c0be82e91a1556697ae7
Danilo <danilopcarlotti@gmail.com>
2019-12-12 15:03:42 -0300

    sumarização de textos

--- common/active_learning_logreg.py
+++ common/active_learning_logreg.py
@@ -3,86 +3,78 @@ from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-import pandas as pd, pymongo, nltk
+import pandas as pd, pymongo, nltk, pickle, os, numpy as np
+
+DATABASE_NAME = 'active_learning_db'
+COLLECTION_NAME = 'document_classification_al'
+PATH_MODEL = os.getcwd()
 
 class active_learning_logreg():
 
-    def __init__(self, N, K, threshold, csv_path, uri_mongo=None):
+    def __init__(self, N, K, threshold_delta, csv_path, path_model_save=PATH_MODEL, uri_mongo=None):
         # N is the number of elements that need to be classified initially for the classification to begin
         self.N = N
         # K is the number of elements that will be manually classified in each interation
         self.K = K
-        # threshold is the minimum accuracy that the model should have before stopping the learning process
-        self.threshold = threshold
+        # threshold_delta is the minimum accuracy that the model should have (difference between 0.5 and the desired)
+        # before stopping the learning process
+        self.threshold_delta = threshold_delta
         # csv_path is the path where the dataset can be found. The csv file should contain only one column, "raw_text"
         self.raw_texts = self.list_texts_raw(csv_path)
         # the texts are transformed in tfidf vectors
         self.normalized_texts_training = self.normalize_texts(self.raw_texts)
         # This database will be created in the mongodb at localhost
-        self.myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        self.mydb = self.myclient['active_learning_db']
+        if not uri_mongo:
+            uri_mongo = "mongodb://localhost:27017/"
+        self.myclient = pymongo.MongoClient(uri_mongo)
+        self.mydb = self.myclient[DATABASE_NAME]
         # This collection will be created in the mongodb
-        self.doc_collection = self.mydb['document_classification_al']
+        self.doc_collection = self.mydb[COLLECTION_NAME]
         # Insert the processed documents in the database for future comparison
-        self.insert_raw_texts(self.normalized_texts_training, self.raw_texts)
+        self.insert_raw_texts()
         # The model that will be constantly updated
         self.vect_fit = None
         self.current_model = None
         self.model_score = None
-    
-    def update_model(self):
-        texts = []
-        for doc in self.doc_collection.find({'class_human': {'$gt':-1}}):
-            texts.append([doc['raw_text'],doc['class_human']])
-        list_texts = self.normalize_texts(texts)
-        vect = TfidfVectorizer()
-        self.vect_fit = vect.fit([i[0] for i in list_texts])
-        tfidf = self.vect_fit.transform([i[0] for i in list_texts])
-        X = tfidf.A
-        y = [i[1] for i in list_texts]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-        self.current_model = LogisticRegression(solver='liblinear',penalty='l1')
-        self.current_model.fit(X_train, y_train)
-        self.model_score = self.current_model.score(X_test, y_test)
-    
-    def insert_raw_texts(self, normalized_texts, raw_texts):
-        for t in range(len(normalized_texts)):
-            self.doc_collection.insert_one({
-                'raw_text':raw_texts[t],
-                'tfidf_vector':normalized_texts[t],
-                'class_human':-1,
-                'class_machine':-1
+        self.path_model_save = path_model_save
+
+    def classify_texts(self):
+        for doc in self.doc_collection.find({'class_human':{'$gt':-1}}):
+            class_prediction = self.current_model.predict([doc['tfidf_vector']])[0]
+            self.doc_collection.update_one({'_id':doc['_id']},{
+                'class_machine':class_prediction
             })
 
+    def dump_model(self):
+        pickle.dump( self.current_model, open(self.path_model_save+'active_learning_model.pickle','wb'))
+
     def find_K_documents(self):
-        texts = []
+        X = []
         for doc in self.doc_collection.find({'class_machine':-1}):
-            texts.append(doc['raw_text'])
-        list_texts = self.normalize_texts(texts)
+            X.append((doc['tfidf_vector'],doc['_id']))
         results = []
-        for lt in list_texts:
-            tfidf = self.vect_fit.transform([lt])
-            X = tfidf.A
-            results.append([lt,self.current_model.predict_proba(X)[0][1]]) # verificar esse resultado
-        results.sort(key=lambda x:x[1])
-        # processar os textos para ranquear e escolher os mais incertos tentando classificar com o modelo treinado
-        # atribuir uma 'class_machine' para todos que puderem ser classificados com uma certeza maior que o threshold
-        # sort a lista e pegar os primeiros K resultados
-        middle = len(results)//2
-        if middle > self.K:
-            most_uncertain = [i[0] for i in results][middle-(self.K//2):middle+(self.K//2)]
-        else:
-            most_uncertain = [i[0] for i in results][middle-(middle//2):middle+(middle//2)]
-        return most_uncertain
+        if not len(X):
+            return False
+        for doc_v, _id in X:
+            results.append([_id, self.current_model.predict_proba([doc_v])[0][1]]) # verificar esse resultado!!
+        most_uncertain = [i[0] for i in results if (i[1] < 0.5 + self.threshold_delta and i[1] > 0.5 - self.threshold_delta)][:self.K]
+        # update documents with to_classify to 1
 
     def find_N_documents(self):
         texts = []
         for doc in self.doc_collection.find({}):
-            if len(texts) < self.N:
-                texts.append(doc['raw_text'])
-            else:
-                break
-        return texts
+            # update document with to_classify to 1
+            pass
+
+    def insert_raw_texts(self):
+        for t in range(len(self.normalized_texts_training)):
+            self.doc_collection.insert_one({
+                'raw_text':self.raw_texts[t],
+                'tfidf_vector':self.normalized_texts_training[t],
+                'class_human':-1,
+                'class_machine':-1,
+                'to_classify':0
+            })
 
     def list_texts_raw(self, csv_path):
         df_raw = pd.read_csv(csv_path, usecols=['raw_text'])
@@ -110,42 +102,54 @@ class active_learning_logreg():
                     except:
                         tokenized_text.append(tkn)
             normal_texts.append(tokenized_text)
-        if one_text:
-            return tokenized_text
-        return normal_texts
+        # return normal_texts
+        # tfidf dos textos
+        vect = TfidfVectorizer()
+        self.vect_fit = vect.fit(normal_texts)
+        tfidf = self.vect_fit.transform(normal_texts)
+        return tfidf.A
     
+    def stop_model_check(self):
+        class_human = []
+        class_machine = []
+        for doc in self.doc_collection.find({'class_machine': {'$gt':-1}}):
+            class_human.append(doc['class_human'])
+            class_machine.append(doc['class_machine'])
+        results = []
+        for i in range(len(class_human)):
+            if class_human[i] == class_machine[i]:
+                results.append(1)
+            else:
+                results.append(0)
+        if np.mean(results) > 0.5 + self.threshold_delta:
+            return True
+        else:
+            return False
 
-input_al = 'csv with one column "raw_text"'
+    def update_model(self):
+        X = []
+        y = []
+        for doc in self.doc_collection.find({'class_human': {'$gt':-1}}):
+            X.append(doc['tfidf_vector'])
+            y.append(doc['class_human'])
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+        self.current_model = LogisticRegression(solver='liblinear', penalty='l1')
+        self.current_model.fit(X_train, y_train)
+        self.model_score = self.current_model.score(X_test,y_test)
+        self.classify_texts()
 
-premissas = '''
-    1) Base de dados aonde se conectar
-    2) X elementos treinados inicialmente, sem ajuda
-    3) A cada interação, classificar manualmente K elementos
-    4) Threshold de certeza Z que o classificador deve ter para todos os elementos para parar o processo
-'''
+if __name__ == "__main__":
+    actv_lrn = active_learning_logreg(20, 10, 0.4, 'csv_path.csv')
+    
+    actv_lrn.find_N_documents()
+    # classify them manually and update documents
 
-etapas = '''
-    1) Treinar manualmente uma base de tamanho X
-    2) Com base no que foi classificado, treinar novamente o classificador com os K novos inputs
-    3) Verificar para todos os elementos existentes quais deles não foram classificados com a certeza mínima Z
-    4) Classificar todos aqueles cuja certeza foi suficientemente grande e deixar separados. Se não houver 
-    elementos, passar para o passo 7
-    5) Disponibilizar os K elementos cuja classificação foi a mais incerta possível para o usuário
-    6) Após reclassificação, voltar ao passo 3
-    7) Exibir: o score da ferramenta de classificação, porcentagem de elementos classificados manualmente
-'''
-
-doc_collection = 'document_classification_al'
-
-single_doc = {
-    # '_id':'random'
-    'tfidf_vector':'texto_processado_na_forma_de_vetor',
-    'class_human':-1,
-    'class_machine':-1
-}
-
-results = {
-    'accuracy':0.0,
-    'texts_manually_classified':0,
-    'classification_ended':False
-}
+    # while Documents to classify
+    while True:
+        docx_to_classify = actv_lrn.find_K_documents()
+        # classify them manually and update document
+        actv_lrn.update_model()
+        if actv_lrn.stop_model_check():
+            break
+    # print(actv_lrn.model_score)
+    actv_lrn.dump_model()
--- common_nlp/classifier_legal_phrases.py
+++ common_nlp/classifier_legal_phrases.py
@@ -1,14 +1,17 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from spacy.lang.pt.examples import sentences 
 from scipy.spatial import distance
-from textNormalization import textNormalization
-import pickle, pandas as pd, numpy as np, re, sys, spacy
+import pickle, pandas as pd, numpy as np, matplotlib.pyplot as plt, re, sys, spacy
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin_min
+try:
+	from textNormalization import textNormalization
+except:
+	from common_nlp.textNormalization import textNormalization
 
 class classifier_legal_phrases():
 	"""classifier_legal_phrases """
-	def __init__(self, list_texts_control, nlp, treshold_distance=None):
+	def __init__(self, list_texts_control, treshold_distance=None):
 		self.list_texts_control = list_texts_control
 		self.vectorizer = TfidfVectorizer()
 		self.txtN = textNormalization()
@@ -36,7 +39,7 @@ class classifier_legal_phrases():
 
 def closest_n_index(X, n_c=10):
 	kmeans = KMeans(n_clusters=n_c, random_state=0).fit(X)
-	closest, aux = pairwise_distances_argmin_min(kmeans.cluster_centers_, X)
+	closest, distances = pairwise_distances_argmin_min(kmeans.cluster_centers_, X)
 	return closest
 
 def closest_n_texts(residual_texts, n_c=10):
@@ -51,36 +54,19 @@ def closest_n_texts(residual_texts, n_c=10):
 		representative_texts.append(residual_texts[i])
 	return representative_texts
 
-def create_classifier(class_name, nlp, df_path='texts_manually_classified.csv'):
+def create_classifier(class_name, df_path='texts_manually_classified.csv'):
 	df = pd.read_csv(df_path)
 	list_of_texts = df[df['class_text'] == class_name]['phrase'].tolist()
-	rel_ph = classifier_legal_phrases(list_of_texts, nlp)
+	rel_ph = classifier_legal_phrases(list_of_texts)
 	pickle.dump(rel_ph,open('classifier_phrases_%s.pickle' % (class_name,),'wb'))
 
 def break_sentences(text, nlp):
 	doc = nlp(text)
 	return [sent.text for sent in doc.sents]
 
-if __name__ == '__main__':
-	language = sys.argv[1]
-	
-	if language == 'pt':
-		nlp = spacy.load('pt_core_news_sm')
-	elif language == 'en':
+def classify_text(text, nlp=None):
+	if not nlp:
 		nlp = spacy.load('en_core_web_sm')
-	else:
-		raise('Language not supported')
-	
-	# CREATE CLASSIFIERS
-	classes = ['fact','claim','decision','law']
-	for c in classes:
-		create_classifier(c, nlp)
-	sys.exit()
-
-	n_c = int(sys.argv[2])
-	text_to_analyse = sys.argv[2]
-	
-
 	# USING CLASSIFIER
 	classifier_facts = pickle.load(open('classifier_phrases_fact.pickle','rb'))
 	classifier_claim = pickle.load(open('classifier_phrases_claim.pickle','rb'))
@@ -94,21 +80,28 @@ if __name__ == '__main__':
 
 	for sentence in break_sentences(text_to_analyse, nlp):
 		res_facts = classifier_facts.belongs_to_class(sentence)
+		res_claim = None
+		res_decisions = None
+		res_laws = None
 		if res_facts:
 			facts.append([sentence,res_facts])
-		res_claim = classifier_claim.belongs_to_class(sentence)
-		if res_claim:
-			claims.append([sentence,res_claim])
-		res_decisions = classifier_decision.belongs_to_class(sentence)
-		if res_decisions:
-			decisions.append([sentence,res_decisions])
-		res_laws = classifier_laws.belongs_to_class(sentence)
-		if res_laws:
-			laws.append([sentence,res_laws])
+		else:
+			res_claim = classifier_claim.belongs_to_class(sentence)
+			if res_claim:
+				claims.append([sentence,res_claim])
+			else:
+				res_decisions = classifier_decision.belongs_to_class(sentence)
+				if res_decisions:
+					decisions.append([sentence,res_decisions])
+				else:
+					res_laws = classifier_laws.belongs_to_class(sentence)
+					if res_laws:
+						laws.append([sentence,res_laws])
 		if not res_laws and not res_decisions and not res_claim and not res_facts:
 			arguments.append(sentence)
 	
-	typical_arguments = closest_n_texts(arguments, n_c=n_c)
+	# typical_arguments = closest_n_texts(arguments, n_c=n_c)
+	typical_arguments = arguments
 	facts.sort(key=lambda x:x[1])
 	claims.sort(key=lambda x:x[1])
 	decisions.sort(key=lambda x:x[1])
@@ -120,4 +113,49 @@ if __name__ == '__main__':
 		'Laws':laws[:n_c],
 		'Arguments':typical_arguments[:n_c]
 	}
-	print(dic_aux)
+	return dic_aux
+
+def classify_sentences(text, path_classifiers='', nlp=None):
+	if not nlp:
+		nlp = spacy.load('en_core_web_sm')
+
+	dictionary_methods = {
+		'fact':pickle.load(open(path_classifiers+'classifier_phrases_fact.pickle','rb')),
+		'claim':pickle.load(open(path_classifiers+'classifier_phrases_claim.pickle','rb')),
+		'decision':pickle.load(open(path_classifiers+'classifier_phrases_decision.pickle','rb')),
+		'law':pickle.load(open(path_classifiers+'classifier_phrases_law.pickle','rb'))
+	}
+
+	classes_sentences = []
+
+	for sentence in break_sentences(text_to_analyse, nlp):
+		sentence_found = False
+		for k,v in dictionary_methods.items():
+			if v.belongs_to_class(sentence):
+				classes_sentences.append(k)
+				sentence_found = True
+				break
+		if not sentence_found:
+			classes_sentences.append('argument')
+	
+	return classes_sentences
+
+if __name__ == '__main__':
+	
+	# # CREATE CLASSIFIERS
+	# classes = ['fact','claim','decision','law']
+	# for c in classes:
+	# 	create_classifier(c)
+	# sys.exit()
+
+	text_to_analyse = sys.argv[1]
+	n_c = int(sys.argv[2])
+	language = sys.argv[3]
+	
+	if language == 'pt':
+		nlp = spacy.load('pt_core_news_sm')
+	elif language == 'en':
+		nlp = spacy.load('en_core_web_sm')
+	else:
+		raise('Language not supported')
+	
